9b41863d68e8071213e9ed1e8f2471be95c35f83
Bachelorarbeit <Bachelorarbeit@Johannes-PC.fritz.box>
2019-08-15 18:19:16 +0200

    -WS1819 Tests 15.08-

--- maven-projektarbeit/src/test/java/GalenFramework/GalenTest.java
+++ maven-projektarbeit/src/test/java/GalenFramework/GalenTest.java
@@ -110,6 +110,19 @@ public class GalenTest {
 
 	}
 
+	public String getElementXPath(WebDriver driver, WebElement element) {
+		String xpath = (String) ((JavascriptExecutor) driver).executeScript(
+				"gPt=function(c){if(c.id!==''){return'id(\"'+c.id+'\")'}if(c===document.body)"
+				+ "{return c.tagName}var a=0;var e=c.parentNode.childNodes;"
+				+ "for(var b=0;b<e.length;b++){var d=e[b];if(d===c)"
+				+ "{return gPt(c.parentNode)+'/'+c.tagName+'['+(a+1)+']'}if(d.nodeType===1&&d.tagName===c.tagName){a++}}};"
+				+ "return gPt(arguments[0]).toLowerCase();",
+				element);
+
+		return "/html/" + xpath;
+
+	}
+	
 	public void aufgabe8() throws IOException {
 
 		String xPathImageRightFloat = getImageWithRightFloat();
@@ -125,14 +138,7 @@ public class GalenTest {
 
 	}
 
-	public String getElementXPath(WebDriver driver, WebElement element) {
-		String xpath = (String) ((JavascriptExecutor) driver).executeScript(
-				"gPt=function(c){if(c.id!==''){return'id(\"'+c.id+'\")'}if(c===document.body){return c.tagName}var a=0;var e=c.parentNode.childNodes;for(var b=0;b<e.length;b++){var d=e[b];if(d===c){return gPt(c.parentNode)+'/'+c.tagName+'['+(a+1)+']'}if(d.nodeType===1&&d.tagName===c.tagName){a++}}};return gPt(arguments[0]).toLowerCase();",
-				element);
 
-		return "/html/" + xpath;
-
-	}
 
 	@Test
 	public void homePageLayoutTest() throws IOException {
